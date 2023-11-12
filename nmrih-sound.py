d7f4cd24edb952d4bcdfd48804e3d4d1b1fcb15b
qJay44 <m70155302@yandex.ru>
2023-11-12 18:24:29 +0300

    updates

--- nmrih-sound.py
+++ nmrih-sound.py
@@ -3,28 +3,26 @@ import subprocess
 import vpk
 import pathlib
 
-VANILA_SOUNDS_FOLDER = 'C:\\Users\\gerku\\Documents\\non-project-trash\\vpk-nmrih\original\\firearms'
-VANILA_DECREASED_SOUNDS_FOLDER = 'C:\\Users\\gerku\\Documents\\non-project-trash\\vpk-nmrih\original\\firearms-decreased'
+VANILA_SOUNDS_FOLDER = 'vpk-nmrih\\original\\firearms'
 ADDON_SOUNDS_FOLDER = 'C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\224260'
-ADDON_DECREASED_SOUNDS_FOLDER = 'vpk-nmrih\\root'
-DECREASE_FACTOR = 0.5
+NEW_SOUNDS_ROOT = 'vpk-nmrih\\root'
+VOLUME_VALUE = 0.2
 
 
 def deacreaseOriginalSounds():
     dirsPath = [os.path.join(VANILA_SOUNDS_FOLDER, name) for name in os.listdir(VANILA_SOUNDS_FOLDER)]
-    os.makedirs(VANILA_DECREASED_SOUNDS_FOLDER, exist_ok=True)
 
     for folderPath in dirsPath:
         for file in os.listdir(folderPath):
             if 'fire' in file and not 'dryfire' in file:
-                folder = folderPath.split(VANILA_SOUNDS_FOLDER)[1]
-                os.makedirs(f'{VANILA_DECREASED_SOUNDS_FOLDER}\{folder}', exist_ok=True)
+                newFilePath = f'{NEW_SOUNDS_ROOT}\\sound\\weapons\\firearms\{folderPath.split(VANILA_SOUNDS_FOLDER)[1]}'
+                os.makedirs(newFilePath, exist_ok=True)
                 subprocess.call(
                     [
                         'ffmpeg', '-hide_banner', '-y',
                         '-i', f'{folderPath}\{file}',
-                        '-filter:a', f'volume={DECREASE_FACTOR}',
-                        f'{VANILA_DECREASED_SOUNDS_FOLDER}{folder}\{file}'
+                        '-filter:a', f'volume={VOLUME_VALUE}',
+                        f'{newFilePath}\{file}'
                     ]
                 )
 
@@ -34,33 +32,33 @@ def deacreaseAddonSounds():
 
     for folderPath in dirsPath:
         for file in os.listdir(folderPath):
+            # Make sure to operate on a vpk file, not its cache
             if file[-3:] == 'vpk':
                 pak = vpk.open(f'{folderPath}\{file}')
-                pak.read_index()
+                pak.read_index() # Init the tree
 
-                # Every existing file and folder in vpk file
-                for k in pak.tree.keys():
-                    if 'sound/weapons/firearms' in k:
-                        path = pathlib.Path(k)
+                # For every existing file and folder in a vpk file
+                for pakFile in pak.tree.keys():
+                    if 'sound/weapons/firearms' in pakFile:
+                        path = pathlib.Path(pakFile)
                         if 'fire' in str(path.stem) and not 'dryfire' in str(path.stem):
-                            os.makedirs(f'{ADDON_DECREASED_SOUNDS_FOLDER}\{path.parent}', exist_ok=True)
-                            tempFile = f'{ADDON_DECREASED_SOUNDS_FOLDER}\{path.parent}\{path.stem}_temp{path.suffix}'
-                            pak.get_file(k).save(tempFile)
+                            os.makedirs(f'{NEW_SOUNDS_ROOT}\{path.parent}', exist_ok=True)
+                            tempFile = f'{NEW_SOUNDS_ROOT}\{path.parent}\{path.stem}_temp{path.suffix}'
+                            pak.get_file(pakFile).save(tempFile)
                             subprocess.call(
                                 [
                                     'ffmpeg', '-hide_banner', '-y',
-                                    '-i',
-                                    tempFile,
-                                    '-filter:a', f'volume={DECREASE_FACTOR}',
+                                    '-i', tempFile,
+                                    '-filter:a', f'volume={VOLUME_VALUE}',
                                     tempFile.replace('_temp', '')
                                 ]
                             )
                             os.remove(tempFile)
 
-    newpak = vpk.new('.\\' + ADDON_DECREASED_SOUNDS_FOLDER)
-    newpak.save('./vpk-nmrih/firearms-decreased-volume-sounds.vpk')
+    newPak = vpk.new(NEW_SOUNDS_ROOT)
+    newPak.save('vpk-nmrih/firearms-decreased-volume-sounds.vpk')
 
 
-deacreaseOriginalSounds()
+# deacreaseOriginalSounds()
 deacreaseAddonSounds()
 
