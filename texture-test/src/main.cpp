aeab9a5a08f297e32974cf7586664751b7f3b370
qJay44 <08gerkules08@gmail.com>
2024-04-06 13:39:23 +0300

    upd

--- texture-test/src/main.cpp
+++ texture-test/src/main.cpp
@@ -1,35 +1,39 @@
-// NOTE: Put a test.tga file and create textures" folder near the exe file
+// NOTE: Put a "tex.png" file near the exe file
 
 #include <iostream>
 #include <ostream>
 #include <string>
+#include <filesystem>
 
 #include "hsv.hpp"
 
-int main() {
-  sf::Clock clock;
+namespace fs = std::filesystem;
 
+int main() {
   sf::Texture tex;
-  tex.loadFromFile("test.tga");
+  tex.loadFromFile("tex.png");
 
   sf::Image img = tex.copyToImage();
   int width = img.getSize().x;
   int height = img.getSize().y;
 
+  constexpr int maxFrames = 50;
+  constexpr int minBrightness = 50;
+  constexpr float rot = 360.f / maxFrames;
+
+  float hue = 0.f;
   int frame = 1;
 
-  float dt;
-  float rot = 36.f;
-  float hue = 0.f;
+  if (!fs::exists("textures")) fs::create_directory("textures");
 
   while (hue < 360.f) {
-    std::cout << "frame: "<< frame << '\r' << std::flush;
+    std::cout << "frame: "<< frame << "/" << maxFrames << '\r' << std::flush;
     sf::Color col = HSV(hue);
 
     for (int i = 0; i < height; i++) {
       for (int j = 0; j < width; j++) {
         sf::Color p = img.getPixel(j, i);
-        if (p.r >= 50 || p.g >= 50 || p.b >= 50)
+        if (p.r >= minBrightness || p.g >= minBrightness || p.b >= minBrightness) // Assuming alpha is always 255
           img.setPixel(j, i, col);
       }
     }
@@ -39,8 +43,7 @@ int main() {
     fileName += ".png";
     img.saveToFile(fileName);
 
-    dt = clock.restart().asSeconds();
-    hue += rot * dt;
+    hue += rot;
     frame++;
   }
 
