9b7eb6834d3e21ab01c58e6f59b5348b473d5043
qJay44 <08gerkules08@gmail.com>
2025-03-01 01:40:12 +0300

    upd

--- sven-coop/config.py
+++ sven-coop/config.py
@@ -1,8 +1,60 @@
-SV_PATH = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Sven Co-op\\'
+import requests
+import pickle
+from os.path import exists
+from os import scandir
 
-MODELS_PATH1    = f'{SV_PATH}\\svencoop_addon\\models\\player'
-MODELS_PATH2    = f'{SV_PATH}\\svencoop_downloads\\models\\player'
-MY_CFG_PATH     = f'{SV_PATH}\\svencoop\\my.cfg'
-KING_CFG_PATH   = f'{SV_PATH}\\svencoop\\king.cfg'
-VC_PATH         = f'{SV_PATH}\\svencoop_downloads\\sound\\vc\\pack'
-KING_SOUND_PATH = f'{SV_PATH}\\svencoop_downloads\\sound\\king'
+SV_PATH = r'C:/Program Files (x86)/Steam/steamapps/common/Sven Co-op/'
+
+MODELS_PATH1    = rf'{SV_PATH}/svencoop_addon/models/player'
+MODELS_PATH2    = rf'{SV_PATH}/svencoop_downloads/models/player'
+MY_CFG_PATH     = rf'{SV_PATH}/svencoop/my.cfg'
+KING_CFG_PATH   = rf'{SV_PATH}/svencoop/king.cfg'
+VC_PATH         = rf'{SV_PATH}/svencoop_downloads/sound/vc/pack'
+
+MODELS = [*scandir(MODELS_PATH1), *scandir(MODELS_PATH2)]
+
+HATS = ['afro', 'angel2', 'angelhead', 'anticross', 'arrow autism', 'aviators', 'azusa_big', 'baron_bunny_new',
+        'beerhat beret', 'booptail', 'camohat', 'cathead', 'cattail chef', 'clocknecklace', 'clown_wig',
+        'collar_pink', 'cophat cowboy', 'cross', 'crown', 'deal_with_it', 'devil2 devilhead', 'deviltail',
+        'devilwing', 'elf', 'fag fox_hat', 'fox_hat _b', 'gasmask_wh', 'goldhead', 'headcrab headphones',
+        'hood', 'inosuke', 'jamacahat2', 'jotaro_hat kermit_cap', 'kfcbucket', 'magic', 'mask', 'mic_chan padoru',
+        'pandahead' , 'paperbag', 'pighead', 'pigtail pirate2', 'pyramidhead_new', 'randoseru', 'randoseru_padoru',
+        'randoseru_s ricefarmer', 'santahat', 'santahat2', 'shades', 'stahlhelm sumb raro2', 'tank', 'tophat', 'tutu',
+        'tweedle ushanka', 'ushanka_2', 'viking', 'wehrmacht', 'wehrmacht2 wing_freedom ']
+
+TRAIL_SPRITES = ['arrows', 'fatline', 'interlace', 'lightning', 'point', 'smokey', 'squarewave', 'svenlogo', 'thinline', 'voice']
+TRAIL_PALETTES = [ 'anime', 'beach', 'cyberpunk', 'forest', 'goldfish', 'interceptor', 'intersex', 'lgbt', 'light', 'random',
+                  'metro', 'moss', 'neonpunk', 'pansexual', 'pastel', 'seaweed', 'sugar', 'trap', 'wheel', 'white', 'winter']
+
+
+def getChatSounds():
+    response = requests.get(r'https://kingsc.net/ChatSounds.txt')
+    if response.status_code != 200:
+        print('Bad response')
+    else:
+        lines = [line.split('\t')[1].split(' ')[0] for line in response.text.splitlines()[3:]]
+        with open('cs.pkl', 'wb') as f:
+            pickle.dump(lines, f)
+
+
+# [yellowygreen | yelloworange | yellowishorange | ...] -> [yellowygreen, yelloworange, yellowishorange, ...]
+def convert(filename):
+    with open(f'{filename}.txt', 'r') as fr:
+        with open(f'{filename}.pkl', 'wb') as fw:
+            pickle.dump(fr.read().replace('\n', '| ').split(' | '), fw)
+
+
+if not exists('cs.pkl'):
+    getChatSounds()
+
+if not exists('colornames.pkl'):
+    # The list itself should in the corresponding .txt file
+    if not exists('colornames.txt'):
+        print('missing colornames.txt')
+        exit(1)
+    convert('colornames')
+
+# Large lists are stored in pickle files
+CHAT_SOUNDS = (lambda: (f:=open('cs.pkl', 'rb'), pickle.load(f), f.close())[1])()
+TRAIL_COLORNAMES = (lambda: (f:=open('colornames.pkl', 'rb'), pickle.load(f), f.close())[1])()
+
--- sven-coop/main.py
+++ sven-coop/main.py
@@ -2,46 +2,32 @@ from os import scandir
 from random import choice, randint
 from config import *
 import json
-import requests
 
+tcolorname = choice(TRAIL_COLORNAMES)
+tpalette = choice(TRAIL_PALETTES)
+tsprite = choice(TRAIL_SPRITES)
+model = choice(MODELS).name
+name = choice(CHAT_SOUNDS)
+hat = choice(HATS)
 
-def chooseSound():
-    response = requests.get("https://kingsc.net/ChatSounds.txt")
-    if response.status_code != 200:
-        print("Bad response")
-
-    lines = [line.split('\t')[1].split(' ')[0] for line in response.text.splitlines()[3:]]
-
-    return choice(lines)
-
-
-def chooseHat():
-    hats = ['afro', 'angel2', 'angelhead', 'anticross', 'arrow autism', 'aviators', 'azusa_big', 'baron_bunny_new', \
-            'beerhat beret', 'booptail', 'camohat', 'cathead', 'cattail chef', 'clocknecklace', 'clown_wig', \
-            'collar_pink', 'cophat cowboy', 'cross', 'crown', 'deal_with_it', 'devil2 devilhead', 'deviltail', \
-            'devilwing', 'elf', 'fag fox_hat', 'fox_hat _b', 'gasmask_wh', 'goldhead', 'headcrab headphones', \
-            'hood', 'inosuke', 'jamacahat2', 'jotaro_hat kermit_cap', 'kfcbucket', 'magic', 'mask', 'mic_chan padoru', \
-            'pandahead' , 'paperbag', 'pighead', 'pigtail pirate2', 'pyramidhead_new', 'randoseru', 'randoseru_padoru', \
-            'randoseru_s ricefarmer', 'santahat', 'santahat2', 'shades', 'stahlhelm sumb raro2', 'tank', 'tophat', 'tutu', \
-            'tweedle ushanka', 'ushanka_2', 'viking', 'wehrmacht', 'wehrmacht2 wing_freedom ']
-
-    return choice(hats)
-
-modelDirs = [*scandir(MODELS_PATH1), *scandir(MODELS_PATH2)]
-model = choice(modelDirs).name
-name = chooseSound()
-hat = chooseHat()
-
-# Write vc voice and pitch to twlz config
+# Server config
 with open(KING_CFG_PATH, 'w', encoding='utf-8') as f:
     vcDirs = [*scandir(VC_PATH)]
     vcVoice = choice(vcDirs).name[:-7]
     vcPitch = randint(60, 150)
-    f.write(f".vc voice {vcVoice}\n.vc pitch {vcPitch}\n.skin -2\n.hat {hat}\n.trail random")
+    scfg = \
+        f'say trail {tcolorname} {tsprite}\n' + \
+        f'.vc voice {vcVoice}\n' + \
+        f'.vc pitch {vcPitch}\n' + \
+        f'.hat {hat}\n' + \
+        f'.trail {tpalette}\n' + \
+         '.skin -2'
+    f.write(scfg)
 
-# Write model command to my.cfg
+# Client config
 with open(MY_CFG_PATH, 'w', encoding='utf-8') as f:
-    f.write(f"model {model}\nname {name}")
+    ccfg = f'model {model}\nname {name}'
+    f.write(ccfg)
 
 # Append used model to json
 with open('used_models.json', 'r+') as f:
@@ -53,11 +39,5 @@ with open('used_models.json', 'r+') as f:
     f.seek(0)
     json.dump(data, f, indent=4)
 
-print(
-    f"model {model}\n"
-    f"name {name}\n"
-    f".vc voice {vcVoice}\n"
-    f".vc pitch {vcPitch}\n"
-    f"hat {hat}\n"
-)
+print(f'{scfg}\n\n{ccfg}')
 
