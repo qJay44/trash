fdc9e5a4df92cbee713e8e334948079fa3dc9f99
qJay44 <08gerkules08@gmail.com>
2025-06-03 20:16:34 +0300

    upd

--- buildGDAL-3.11.0/buildGDAL.bat
+++ buildGDAL-3.11.0/buildGDAL.bat
@@ -6,11 +6,11 @@ cd build
 
 cmake .. ^
 -DCMAKE_INSTALL_PREFIX="install" ^
--DCMAKE_PREFIX_PATH="%LIBS%/proj-9.6.0/build/install" ^
--DPROJ_INCLUDE_DIR="%LIBS%/proj-9.6.0/build/install/include" ^
--DPROJ_LIBRARY_RELEASE="%LIBS%/proj-9.6.0/build/install/lib/libproj.dll.a" ^
--DTIFF_INCLUDE_DIR="%LIBS%/tiff-4.7.0/install/include" ^
--DTIFF_LIBRARY_RELEASE="%LIBS%/tiff-4.7.0/install/lib/tiff.lib" ^
+-DCMAKE_PREFIX_PATH="%LIBS%/proj-9.6.1/build/install" ^
+-DPROJ_INCLUDE_DIR="%LIBS%/proj-9.6.1/build/install/include" ^
+-DPROJ_LIBRARY_RELEASE="%LIBS%/proj-9.6.1/build/install/lib/libproj.dll.a" ^
+-DTIFF_INCLUDE_DIR="%LIBS%/tiff-4.7.0/build/install/include" ^
+-DTIFF_LIBRARY_RELEASE="%LIBS%/tiff-4.7.0/build/install/lib/tiff.lib" ^
 -DGDAL_USE_SQLITE3=ON ^
 -DSQLite3_INCLUDE_DIR="%LIBS%/sqlite-amalgamation-3500000" ^
 -DSQLite3_LIBRARY="%LIBS%/sqlite-amalgamation-3500000/libsqlite3.a" ^
