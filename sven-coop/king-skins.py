65786db6bd4a0dec08c037262b535b2b647e6076
qJay44 <08gerkules08@gmail.com>
2025-06-08 17:10:05 +0300

    upd

--- sven-coop/king-skins.py
+++ sven-coop/king-skins.py
@@ -1,8 +1,13 @@
 def generate(linesNum: int):
-    cmds = "alias _skin _skin1\n"
-    for line in range(1, linesNum + 1):
-        cmds += f'alias _skin{line} ".skin -{line}; alias _skin _skin{1 if line == linesNum else line + 1}"\n'
+    cmds = f"alias _skin_prev _skin{linesNum}; alias _skin_next _skin1\n\n"
+    width = len(str(linesNum))
+    for line in range(linesNum + 1):
+        currName = f'{line:<{width}}'
+        currValue = f'-{currName}'
+        prevValue = f'{(linesNum + line) % (linesNum + 1):<{width}}'
+        nextValue = f'{(line + 1) % (linesNum + 1)}'
 
+        cmds += f'alias _skin{currName} ".skin {currValue}; alias _skin_prev _skin{prevValue}; alias _skin_next _skin{nextValue}\n'
 
     with open('king-skins.cfg', 'w') as f:
         f.write(cmds)
